332c9e48d4784441e893fdf437987bb111ef2224
andreperna <andreperna@gmail.com>
2022-06-21 11:19:13 -0300

    upadate

---
+++ main.py
@@ -0,0 +1,28 @@
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class CategoriaIdade(str, Enum):
+    crianca1 = 'crianca2'
+    adulto1 = 'adulto2'
+    idoso1 = 'idoso2'
+
+@app.get('/')
+def root():
+    return {'msg':'Hello World'}
+
+@app.get('/items/{item}')
+def root(item:int):
+    return {'msg':item}
+
+@app.get('/cat-idade/{cat_idade}')
+def idade(cat_idade:CategoriaIdade):
+    return {'categoria idade':cat_idade.capitalize(), 'name':cat_idade.name}
+
+@app.get('/query/')
+def query(cidade:str=None, limit:int=10):
+    return {
+        'cidade': cidade,
+        'limite': limit
+    }
